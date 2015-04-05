ee9c1b01a853ac7d012cceb7e74a510b8ff7f3e6
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-04-05 13:54:44 +0900

    fix comment error

--- setupTex.sh
+++ setupTex.sh
@@ -6,33 +6,36 @@ brew install caskroom/cask/brew-cask
 brew cask install basictex
 brew install nkf ghostscript
 
+echo "export PATH=/usr/texbin:$PATH" >> ~/.bash_profile
+source ~/.bash_profile
+
 sudo tlmgr update --self --all
 sudo tlmgr install ptex ptex2pdf jfontmaps jsclasses japanese-otf boxedminipage epsf c
 
 # memo: - change texshop setting default to ptex(ptex2pdf)?
 brew cask install texshop
 
-echo "add 'export PATH=/usr/texbin:$PATH' to shell profile"
-echo "add following function"
-echo "	function tex(){"
-echo "		FILESTRING=$1"
-echo "		FILENAME=${FILESTRING%.*}"
-echo ""
-echo "		VAR=`nkf -g ${FILENAME}.tex`"
-echo "		if [ "${VAR}" = "Shift_JIS" ]; then"
-echo "			platex -kanji=sjis ${FILENAME}.tex"
-echo "			jbibtex -kanji=sjis ${FILENAME}"
-echo "			platex -kanji=sjis ${FILENAME}.tex"
-echo "			platex -kanji=sjis ${FILENAME}.tex"
-echo "			dvipdfmx ${FILENAME}.dvi"
-echo "			open ${FILENAME}.pdf"
-echo "		elif [ "${VAR}" = "EUC-JP" ]; then"
-echo "			platex -kanji=euc ${FILENAME}.tex"
-echo "			jbibtex -kanji=euc ${FILENAME}"
-echo "			platex -kanji=euc ${FILENAME}.tex"
-echo "			platex -kanji=euc ${FILENAME}.tex"
-echo "			dvipdfmx ${FILENAME}.dvi"
-echo "			open ${FILENAME}.pdf"
-echo "		fi"
-echo "		echo ${VAR}"
-echo "	}"
+echo "And add following function
+	function tex(){
+		FILESTRING=$1
+		FILENAME=${FILESTRING%.*}
+
+		VAR=`nkf -g ${FILENAME}.tex`
+		if [ "${VAR}" = "Shift_JIS" ]; then
+			platex -kanji=sjis ${FILENAME}.tex
+			jbibtex -kanji=sjis ${FILENAME}
+			platex -kanji=sjis ${FILENAME}.tex
+			platex -kanji=sjis ${FILENAME}.tex
+			dvipdfmx ${FILENAME}.dvi
+			open ${FILENAME}.pdf
+		elif [ "${VAR}" = "EUC-JP" ]; then
+			platex -kanji=euc ${FILENAME}.tex
+			jbibtex -kanji=euc ${FILENAME}
+			platex -kanji=euc ${FILENAME}.tex
+			platex -kanji=euc ${FILENAME}.tex
+			dvipdfmx ${FILENAME}.dvi
+			open ${FILENAME}.pdf
+		fi
+		echo ${VAR}
+	}" >> ~/.bash_profile
+source ~/.bash_profile

fbd5693372c18453b46d40861ec4c0588c03c5b1
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2015-10-02 13:33:26 +0900

    update

--- setup.sh
+++ setup.sh
@@ -4,27 +4,16 @@
 # - iCloudサインアウト（Macを探すの解除）
 
 ## afeter
-# App Store
 # Xcode, Line
-
-# Other
-# MSOffice, Adobe
+# MSOffice, Adobe, Locamatic
 # .ssh, photos, iTunes
 
+# sudo xcodebuild -license
+# xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew doctor
 brew install caskroom/cask/brew-cask
-
-brew tap sanemat/font
-brew install ricty
-cp -f /usr/local/Cellar/ricty/3.2.3/share/fonts/Ricty*.ttf ~/Library/Fonts/
-brew reinstall --powerline --vim-powerline ricty
-cp -f /usr/local/Cellar/ricty/3.2.4/share/fonts/Ricty*.ttf ~/Library/Fonts/
-fc-cache -vf
-
-mkdir -p ~/.vim/bundle
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 # Packages for development
 brew install zsh
@@ -40,15 +29,24 @@ brew install trash
 brew-cask install google-chrome
 brew-cask install dropbox
 brew-cask install iterm2
-brew-cask install xquartz
 brew-cask install caffeine
-brew-cask install karabiner
 brew-cask install google-japanese-ime
 brew-cask install skype
-
-ln -snf ~/Dropbox/Backup/SymbolicLink/dotfiles/.zshrc ~/
-ln -snf ~/Dropbox/Backup/SymbolicLink/dotfiles/.zshrc.private ~/
-ln -snf ~/Dropbox/Backup/SymbolicLink/dotfiles/.vimrc ~/
+brew-cask install atom
+brew-cask install karabiner
+brew-cask install xquartz
 
 brew tap peco/peco
 brew install peco
+
+brew tap sanemat/font
+brew install ricty
+brew reinstall --powerline --vim-powerline ricty
+cp -f /usr/local/Cellar/ricty/3.2.4/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
+
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+sudo easy_install pip
+sudo pip install beautifulsoup4
---
+++ setupRC.sh
@@ -0,0 +1,4 @@
+ln -snf ~/Dropbox/Backup/settings/.zshrc ~/
+ln -snf ~/Dropbox/Backup/settings/.zshrc.private ~/
+ln -snf ~/Dropbox/Backup/settings/.vimrc ~/
+

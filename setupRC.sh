c5cc5fc554579f610f97d3e0c91a0c52861a4025
Yushi Uchimura <yuchimur@yahoo-corp.jp>
2017-03-10 19:04:36 +0900

    Updated: introduce prezto/https://github.com/ir77/prezto

--- setupRC.sh
+++ setupRC.sh
@@ -2,7 +2,11 @@ mkdir -p ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/dein.toml" ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/dein_lazy.toml" ~/.vim/rc
 
-ln -snf "$(cd $(dirname $0) && pwd)/.zshrc" ~/
 ln -snf "$(cd $(dirname $0) && pwd)/.vimrc" ~/
 ln -snf "$(cd $(dirname $0) && pwd)/.tmux.conf" ~/
 
+git clone --recursive https://github.com/ir77/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done

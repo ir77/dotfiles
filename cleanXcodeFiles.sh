ef853e33d5b06bf05cc84595d60242f942be999f
Yushi Uchimura <yuchimur@yahoo-corp.jp>
2018-02-26 13:24:44 +0900

    Added: clean xcode files script

---
+++ cleanXcodeFiles.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# 動作確認: 2018/02/26
+# [Xcode 8.2] 年末なので Xcode まわりの不要ファイルを一掃してみた ｜ Developers.IO
+# https://dev.classmethod.jp/smartphone/remove-xcode8-related-unnecessary-files/
+
+# 実機デバッグを行うために必要なファイルを削除
+rm -r ~/Library/Developer/Xcode/iOS\ DeviceSupport/*
+
+# プロジェクトのインデックスやビルド時の生成物、ログなどを削除
+rm -r ~/Library/Developer/Xcode/DerivedData/*
+
+# Product -> Archive 実行時に作成されるファイルを削除
+rm -r ~/Library/Developer/Xcode/Archives/*
+
+# シミュレータ上にインストールしたアプリや設定を削除
+# simctl: シミュレータの制御ツール
+xcrun simctl erase all
+
+# SDK のドキュメント等を削除
+rm -r ~/Library/Developer/Shared/Documentation/DocSets/*
+
+# デバイスログを削除
+rm -r ~/Library/Developer/Xcode/iOS\ Device\ Logs/*

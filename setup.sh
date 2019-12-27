5aea1c8acee840385557dce2b6063d95755cc641
yuchimur <yuchimur@yahoo-corp.jp>
2019-12-27 10:10:41 +0900

    update

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ brew install carthage
 
 brew cask install google-chrome
 brew cask install caffeine
-brew cask install google-japanese-ime
+brew cask install google-japanese-ime # 設定後に再起動の必要あり
 brew cask install karabiner-elements
 brew cask install xquartz
 brew cask install appcleaner
@@ -55,7 +55,6 @@ brew cask install iterm2
 brew cask install shiftit
 brew cask install sketch
 brew cask install dropbox
-brew cask install atom
 brew cask install paste
 brew cask install kindle
 brew cask install visual-studio-code
@@ -69,12 +68,6 @@ brew cask install slack
 brew tap homebrew/cask-versions
 brew cask install java11
 
-# brew tap sanemat/font
-# brew install ricty
-# brew reinstall ricty --with-powerline
-# cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-# fc-cache -vf
-
 # brew search powerline 他のが気になったらこれで探す
 # brew install homebrew/cask-fonts/font-anonymice-powerline
 # brew install homebrew/cask-fonts/font-consolas-for-powerline
@@ -84,18 +77,9 @@ brew cask install java11
 brew install homebrew/cask-fonts/font-roboto-mono-for-powerline
 
 sudo gem install cocoapods
-# sudo gem install tldrb
 
 # git clone https://github.com/riywo/anyenv ~/.anyenv
 # anyenv install rbenv
-
-git clone https://github.com/tomislav/osx-terminal.app-colors-solarized
-echo 'set terminal colors to solarized'
-
-# defaults delete NSGlobalDomain KeyRepeat ＃Defaultに戻す
-defaults write NSGlobalDomain KeyRepeat -int 1
-# defaults delete NSGlobalDomain InitialKeyRepeat # Defaultに戻す
-defaults write NSGlobalDomain InitialKeyRepeat -int 1
 
 # Xcodeにビルド時間を表示する
 defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
@@ -108,12 +92,6 @@ stack setup
 
 echo 'git settings'
 git config --global push.default matching
-git config --global alias.glog "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
-git config --global alias.cm "commit -m"
-git config --global alias.st "status --branch --short"
-git config --global alias.co "checkout"
-git config --global alias.fp "fetch --prune"
-git config --global alias.delete-branch-all "!f () { git branch | grep $1 | xargs git branch -D; }; f"
 git config --global push.default simple # git pushでcurrentブランチだけアップデートする
 git config --global core.editor vim
 

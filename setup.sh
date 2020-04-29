767779d66cb37fc8072cb4e3202227da151c5e79
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-04-29 22:55:02 +0900

    git configを追加

--- setup.sh
+++ setup.sh
@@ -166,6 +166,7 @@ echo 'git settings'
 git config --global push.default matching
 git config --global push.default simple # git pushでcurrentブランチだけアップデートする
 git config --global core.editor vim
+git config --global core.quotepath false # git statusで日本語文字の文字化けを防ぐ
 
 echo 'git config --global user.name "user"'
 echo 'git config --global user.email "email"'

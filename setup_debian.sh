6cb79be666562dce900119955f16f8031bd61c95
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-05 21:09:25 +0900

    add

---
+++ setup_debian.sh
@@ -0,0 +1,7 @@
+sudo apt-get install dbus
+sudo timedatectl set-timezone Asia/Tokyo // timezoneの変更
+
+git config --global user.email fly.oh.shogun.iridium.77@gmail.com
+git config --global user.name ir77
+
+curl -fsSL https://starship.rs/install.sh | bash // starship

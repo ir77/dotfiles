44687eca07444e98e4d0e24d3169bbc57cbede27
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-07 23:18:12 +0900

    add

--- setup_debian.sh
+++ setup_debian.sh
@@ -2,10 +2,17 @@ sudo apt-get install gcc
 sudo apt-get install dbus
 sudo timedatectl set-timezone Asia/Tokyo // timezoneの変更
 
+
+
 sudo apt-get install zsh
 curl -fsSL https://starship.rs/install.sh | bash // starship
 echo 'eval "$(starship init zsh)"' > ~/.zshrc
 sudo chsh $USER -s $(which zsh)
+
+// setup mosh
+sudo apt-get install mosh
+sudo apt-get install ufw
+sudo ufw allow 60000:61000/udp
 
 // install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh

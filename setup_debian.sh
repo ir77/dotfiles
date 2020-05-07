718a16448042e0b1e266dee48232ef2d777ebaf5
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-07 20:28:32 +0900

    add

--- setup_debian.sh
+++ setup_debian.sh
@@ -1,7 +1,11 @@
 sudo apt-get install dbus
 sudo timedatectl set-timezone Asia/Tokyo // timezoneの変更
 
+sudo apt-get install zsh
+curl -fsSL https://starship.rs/install.sh | bash // starship
+echo 'eval "$(starship init zsh)"' > ~/.zshrc
+sudo chsh $USER -s $(which zsh)
+
 git config --global user.email fly.oh.shogun.iridium.77@gmail.com
 git config --global user.name ir77
 
-curl -fsSL https://starship.rs/install.sh | bash // starship

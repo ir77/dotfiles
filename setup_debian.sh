ca49a7c4b2994691a78ab37f2c8989b68d1dd255
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-07 20:34:03 +0900

    add

--- setup_debian.sh
+++ setup_debian.sh
@@ -6,6 +6,10 @@ curl -fsSL https://starship.rs/install.sh | bash // starship
 echo 'eval "$(starship init zsh)"' > ~/.zshrc
 sudo chsh $USER -s $(which zsh)
 
+// install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+
 git config --global user.email fly.oh.shogun.iridium.77@gmail.com
 git config --global user.name ir77
 

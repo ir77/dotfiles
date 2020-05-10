9aef9180a5ee2e300343f76ec8251a7994a4cd9d
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-10 12:54:12 +0900

    update coc-settings

--- setupRC.sh
+++ setupRC.sh
@@ -9,3 +9,4 @@ mkdir -p ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/dein.toml" ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/dein_lazy.toml" ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/.vimrc" ~/
+ln -snf "$(cd $(dirname $0) && pwd)/coc-settings.json" ~/.vim

2f50e84bf0b1bb587da6894ee9afc33e28e9c411
yuchimur <yuchimur@yahoo-crop.jp>
2020-05-11 09:43:05 +0900

    ファイル分割

--- setup.sh
+++ setup.sh
@@ -55,9 +55,6 @@ brew cask install deepl
 mas install 961632517 #Be Focused Pro (1.7.9)
 mas install 967805235 #Paste (2.6.2)
 
-brew tap homebrew/cask-versions
-brew cask install java11
-
 # brew search powerline 他のが気になったらこれで探す
 # brew install homebrew/cask-fonts/font-anonymice-powerline
 # brew install homebrew/cask-fonts/font-consolas-for-powerline
@@ -66,68 +63,14 @@ brew cask install java11
 # brew install homebrew/cask-fonts/font-liberation-mono-for-powerline
 brew install homebrew/cask-fonts/font-roboto-mono-for-powerline
 
-sudo gem install cocoapods
-
 anyenv init
 
-# Xcodeにビルド時間を表示する
-defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
+# Java Setup
+brew tap homebrew/cask-versions
+brew cask install java11
 
-# スクリーンショットを英語に
-defaults write com.apple.screencapture name ""
-
-# Dock
-defaults write com.apple.dock persistent-apps -array #Dock に標準で入っている全てのアプリを消す、Finder とごみ箱は消えない
-defaults write com.apple.dock autohide -bool true
-killall Dock
-
-# keyboard
-defaults write -g com.apple.keyboard.fnState -bool true
-
-# finder
-defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.finder ShowPathbar -bool true
-defaults write com.apple.finder QuitMenuItem -bool true
-
-# trackpad 
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
-# Spotlight
-defaults write com.apple.spotlight orderedItems -array \
-    '{"enabled"=1;"name"="APPLICATIONS";}' \
-    '{"enabled"=1;"name"="MENU_CONVERSION";}' \
-    '{"enabled"=1;"name"="MENU_EXPRESSION";}' \
-    '{"enabled"=1;"name"="MENU_DEFINITION";}' \
-    '{"enabled"=1;"name"="SYSTEM_PREFS";}' \
-    '{"enabled"=1;"name"="BOOKMARKS";}' \
-    '{"enabled"=1;"name"="MENU_OTHER";}' \
-    '{"enabled"=0;"name"="MENU_SPOTLIGHT_SUGGESTIONS";}' \
-    '{"enabled"=0;"name"="DOCUMENTS";}' \
-    '{"enabled"=0;"name"="DIRECTORIES";}' \
-    '{"enabled"=0;"name"="PRESENTATIONS";}' \
-    '{"enabled"=0;"name"="SPREADSHEETS";}' \
-    '{"enabled"=0;"name"="PDF";}' \
-    '{"enabled"=0;"name"="MESSAGES";}' \
-    '{"enabled"=0;"name"="CONTACT";}' \
-    '{"enabled"=0;"name"="EVENT_TODO";}' \
-    '{"enabled"=0;"name"="IMAGES";}' \
-    '{"enabled"=0;"name"="MUSIC";}' \
-    '{"enabled"=0;"name"="MOVIES";}' \
-    '{"enabled"=0;"name"="FONTS";}' \
-    '{"enabled"=0;"name"="SOURCE";}' \
-
-## index再生成
-killall mds 
-sudo mdutil -i on /
-sudo mdutil -E /
-
-# バッテリーのパーセントを表示する
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
-# Kill affected applications
-for app in Finder Dock SystemUIServer; do killall "$app" >/dev/null 2>&1; done
+# Xcode Setup
+sudo gem install cocoapods
 
 # Haskell Setup
 stack setup
@@ -141,11 +84,3 @@ nodebrew install-binary stable
 nodebrew ls | xargs nodebrew use
 npm install -g yarn
 
-echo 'git settings'
-git config --global push.default matching
-git config --global push.default simple # git pushでcurrentブランチだけアップデートする
-git config --global core.editor vim
-git config --global core.quotepath false # git statusで日本語文字の文字化けを防ぐ
-
-echo 'git config --global user.name "user"'
-echo 'git config --global user.email "email"'

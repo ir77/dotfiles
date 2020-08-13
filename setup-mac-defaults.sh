14f3db960ad02f5123726f00ce2199e723798d04
ucchy <ir77@users.noreply.github.com>
2020-08-13 16:19:53 +0900

    Update setup-mac-defaults.sh

--- setup-mac-defaults.sh
+++ setup-mac-defaults.sh
@@ -22,30 +22,6 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-# Spotlight
-defaults write com.apple.spotlight orderedItems -array \
-    '{"enabled"=1;"name"="APPLICATIONS";}' \
-    '{"enabled"=1;"name"="MENU_CONVERSION";}' \
-    '{"enabled"=1;"name"="MENU_EXPRESSION";}' \
-    '{"enabled"=1;"name"="MENU_DEFINITION";}' \
-    '{"enabled"=1;"name"="SYSTEM_PREFS";}' \
-    '{"enabled"=1;"name"="BOOKMARKS";}' \
-    '{"enabled"=1;"name"="MENU_OTHER";}' \
-    '{"enabled"=0;"name"="MENU_SPOTLIGHT_SUGGESTIONS";}' \
-    '{"enabled"=0;"name"="DOCUMENTS";}' \
-    '{"enabled"=0;"name"="DIRECTORIES";}' \
-    '{"enabled"=0;"name"="PRESENTATIONS";}' \
-    '{"enabled"=0;"name"="SPREADSHEETS";}' \
-    '{"enabled"=0;"name"="PDF";}' \
-    '{"enabled"=0;"name"="MESSAGES";}' \
-    '{"enabled"=0;"name"="CONTACT";}' \
-    '{"enabled"=0;"name"="EVENT_TODO";}' \
-    '{"enabled"=0;"name"="IMAGES";}' \
-    '{"enabled"=0;"name"="MUSIC";}' \
-    '{"enabled"=0;"name"="MOVIES";}' \
-    '{"enabled"=0;"name"="FONTS";}' \
-    '{"enabled"=0;"name"="SOURCE";}' \
-
 ## index再生成
 killall mds 
 sudo mdutil -i on /

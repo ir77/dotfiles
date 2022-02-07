b1db07fc8a3800272abe1bd02390923f6eecb9b4
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2022-02-07 23:08:39 +0900

    add

--- setup1-brew.sh
+++ setup1-brew.sh
@@ -34,8 +34,9 @@ brew install mysql
 brew install nodebrew
 brew install rustup-init
 brew install deno
-brew install starship
 
+brew install starship
+brew install font-roboto-mono-nerd-font
 
 brew install --cask google-chrome
 brew install --cask google-japanese-ime # 設定後に再起動の必要あり
@@ -51,14 +52,6 @@ brew install --cask intellij-idea
 brew install --cask android-studio
 brew install --cask flutter
 
-# brew search powerline 他のが気になったらこれで探す
-# brew install homebrew/cask-fonts/font-anonymice-powerline
-# brew install homebrew/cask-fonts/font-consolas-for-powerline
-# brew install homebrew/cask-fonts/font-fira-mono-for-powerline
-# brew install homebrew/cask-fonts/font-inconsolata-dz-for-powerline
-# brew install homebrew/cask-fonts/font-liberation-mono-for-powerline
-brew install svn
-brew install homebrew/cask-fonts/font-roboto-mono-for-powerline
 
 anyenv init
 

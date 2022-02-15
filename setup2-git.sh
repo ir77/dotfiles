21008c2d86836a09296acf8c09b1ab742271eaab
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2022-02-15 23:40:55 +0900

    add

--- setup2-git.sh
+++ setup2-git.sh
@@ -3,6 +3,7 @@ git config --global push.default matching
 git config --global push.default simple # git pushでcurrentブランチだけアップデートする
 git config --global core.editor vim
 git config --global core.quotepath false # git statusで日本語文字の文字化けを防ぐ
+git config --global init.defaultBranch main
 
 echo 'git config --global user.name "user"'
 echo 'git config --global user.email "email"'

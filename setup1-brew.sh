54cb2b78f5c69345d8d8314358de982a8e9fc631
ucchy 内村 <ir77@users.noreply.github.com>
2022-04-20 10:49:34 +0900

    Update setup1-brew.sh

--- setup1-brew.sh
+++ setup1-brew.sh
@@ -41,7 +41,8 @@ brew install --cask google-chrome
 brew install --cask google-japanese-ime # 設定後に再起動の必要あり
 brew install --cask caffeine
 brew install --cask karabiner-elements
-brew install --cask shiftit
+brew install --cask shiftit # spectacleで良ければ消すかも
+brew install --cask spectacle
 brew install --cask kindle
 brew install --cask iterm2
 brew install --cask slack

f548503d2d607b8ea61cd23e35d8ae165efd3261
yuchimur <yuchimur@yahoo-corp.jp>
2022-12-11 17:28:03 +0900

    update setup rc

--- setup4-rc.sh
+++ setup4-rc.sh
@@ -5,8 +5,13 @@ ln -snf "$(cd $(dirname $0) && pwd)/.zshrc" ~/
 mkdir -p ~/.config/zsh 
 ln -snf "$(cd $(dirname $0) && pwd)/abbreviations" ~/.config/zsh/abbreviations
 
+# Setup Vim
 mkdir -p ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/dein.toml" ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/dein_lazy.toml" ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/.vimrc" ~/
 ln -snf "$(cd $(dirname $0) && pwd)/coc-settings.json" ~/.vim
+
+# Setup Other
+ln -snf "$(cd $(dirname $0) && pwd)/starship.toml" ~/.config
+

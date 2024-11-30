1aa5c2efa48dd0cc51deb1d8ec835b558ea79aae
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2024-11-30 22:16:01 +0900

    delete zsh-completion

--- setup1-brew.sh
+++ setup1-brew.sh
@@ -49,7 +49,6 @@ brew update
 brew doctor
 
 install_if_not_installed "starship"
-install_if_not_installed "zsh-completions"
 install_if_not_installed "zsh-syntax-highlighting"
 install_if_not_installed "olets/tap/zsh-abbr"
 install_if_not_installed "ack"

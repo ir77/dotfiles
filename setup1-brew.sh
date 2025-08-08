c0b12341f5bda8da6fc143466d1280e553f8470c
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-08-08 14:03:36 +0900

    update brew file

--- setup1-brew.sh
+++ setup1-brew.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-print_message() {
-    local color=$1
-    local message=$2
 
-    case $color in
-        "red")    echo "\033[31m${message}\033[0m" ;;  # 赤
-        "green")  echo "\033[32m${message}\033[0m" ;;  # 緑
-        "yellow") echo "\033[33m${message}\033[0m" ;;  # 黄色
-        *)        echo "${message}" ;;                   # デフォルト
-    esac
-    echo ""  # 改行を追加
+readonly RED='\033[31m'
+readonly GREEN='\033[32m'
+readonly YELLOW='\033[33m'
+readonly NC='\033[0m' # No Color
+
+print_message() {
+  local color="$1"
+  local message="$2"
+  printf "${color}%s${NC}\n" "$message"
 }
 
-xcode-select --install
+if ! xcode-select -p &>/dev/null; then
+  print_message "$YELLOW" "Xcode Command Line Tools not found. Installing..."
+  xcode-select --install
+else
+  print_message "$GREEN" "Xcode Command Line Tools are already installed."
+fi
 
 if ! command -v brew &>/dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+  print_message "$YELLOW" "Homebrew not found. Installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
-    print_message "green" "Homebrew is already installed."
+  print_message "$GREEN" "Homebrew is already installed. Updating..."
 fi
 
 brew update
@@ -26,12 +31,12 @@ brew doctor
 brew bundle
 
 if ! nodebrew ls | grep -q "v"; then
-    print_message "yellow" "Node.js is not installed via Nodebrew. Installing the latest stable version..."
-    mkdir -p ~/.nodebrew/src
-    nodebrew install-binary stable
-    nodebrew ls | xargs nodebrew use
+  print_message "$YELLOW" "Node.js not installed via Nodebrew. Installing the latest stable version..."
+  mkdir -p ~/.nodebrew/src
+  nodebrew install-binary stable
+  nodebrew use latest
 else
-    print_message "green" "Node.js is already installed via Nodebrew."
+  print_message "$GREEN" "Node.js is already installed via Nodebrew."
 fi
 
-print_message "green" "Setup completed!"
+print_message "$GREEN" "Setup completed!"

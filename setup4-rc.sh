806c09866408a87db970454188466acf8a64c409
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-08-12 16:24:54 +0900

    remove dein lazy 2

--- setup4-rc.sh
+++ setup4-rc.sh
@@ -8,7 +8,6 @@ ln -snf "$(cd $(dirname $0) && pwd)/abbreviations" ~/.config/zsh/abbreviations
 # Setup Vim
 mkdir -p ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/dein.toml" ~/.vim/rc
-ln -snf "$(cd $(dirname $0) && pwd)/dein_lazy.toml" ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/.vimrc" ~/
 ln -snf "$(cd $(dirname $0) && pwd)/coc-settings.json" ~/.vim
 

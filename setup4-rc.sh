718cef5b1f39370e230f7ac5b9ec48a3ac738415
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-08-15 22:59:50 +0900

    delete coc settings for vim

--- setup4-rc.sh
+++ setup4-rc.sh
@@ -9,7 +9,6 @@ ln -snf "$(cd $(dirname $0) && pwd)/abbreviations" ~/.config/zsh/abbreviations
 mkdir -p ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/dein.toml" ~/.vim/rc
 ln -snf "$(cd $(dirname $0) && pwd)/.vimrc" ~/
-ln -snf "$(cd $(dirname $0) && pwd)/coc-settings.json" ~/.vim
 
 # Setup Other
 ln -snf "$(cd $(dirname $0) && pwd)/starship.toml" ~/.config

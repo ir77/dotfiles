7c2a49a0c10dcc2a1956ebdb3fb97f965e95bc25
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-08-30 13:18:02 +0900

    update setup brew

--- setup1-brew.sh
+++ setup1-brew.sh
@@ -29,5 +29,6 @@ brew update
 brew upgrade
 brew doctor
 brew bundle
+brew bundle dump --force --file=- | grep -v "^vscode " > Brewfile # 結果を標準出力に強制, vscodeの拡張はvscode側で管理しているので除く
 
 print_message "$GREEN" "Setup completed!"

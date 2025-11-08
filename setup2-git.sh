eaef5cbb027b4b42227840ed77dc79c0363970a5
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2025-11-08 22:01:37 +0900

    Brewfileにgit-deltaを追加し、setup2-git.shでgitの設定を強化

--- setup2-git.sh
+++ setup2-git.sh
@@ -6,6 +6,15 @@ git config --global core.editor vim
 git config --global core.quotepath false # git statusで日本語文字の文字化けを防ぐ
 git config --global init.defaultBranch main
 
+git config --global core.pager delta
+git config --global interactive.diffFilter 'delta --color-only'
+git config --global merge.conflictStyle zdiff3
+git config --global delta.navigate true
+git config --global delta.side-by-side true
+git config --global delta.line-numbers true
+git config --global delta.hunk-header-style "omit"
+git config --global delta.color-moved true
+
 echo 'git config --global user.name "user"'
 echo 'git config --global user.email "email"'
 
